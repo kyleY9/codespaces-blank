63679d4c5b1f44ef6b722a162a5570cccc8f1555
kyleY9 <146113920+kyleY9@users.noreply.github.com>
2023-12-14 17:03:57 +0000

    first commit

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+    int iterations1 = 0;
+    int iterations2 = 0;
+
+    for (int i = 0; i < 5; i++) {
+        iterations1++;
+    }
+    for (int i = 0; i <=5; i++) {
+        iterations2++;
+    }
+    
+    System.out.println(iterations1 + "\n" + iterations2);
+    }
+}
